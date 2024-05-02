5f541540ee3f0bf9921c128d06eef623087012c2
MRHOOH <168110749+MRHOOH@users.noreply.github.com>
2024-05-02 18:44:44 +0700

    Add files via upload

---
+++ Linux999.sh
@@ -0,0 +1,150 @@
+#Linux999
+#Debian
+
+sleep 1
+clear
+sleep 1
+echo -e "      ⠀⠀⠀⠀⠀⠀⠀⠀⠀              ⠀⣀⣤⣤⣤⣤⣤⣤⣀⡀"⠀⠀⠀⠀⠀⠀⠀
+echo -e " ⠀  ⠀⠀   ⠀⠀⠀             ⢀⣠⣶⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣷⣦⣄⣀⣤⣶⣶"
+echo -e " ⠀⠀⠀  ⠀                ⠀⣰⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⠟⠋"
+echo -e " ⠀⠀⠀    ⠀              ⣼⣿⡿⠃⠀⢀⣤⣾⣿⣿⣿⣿⣷⣦⣄⠀⠀⠈⠉   "⠀⠀⠀
+echo -e " ⠀  ⠀                ⠀⣸⣿⡿⠁⠀⢠⣿⣿⠟⠉⠀⠈⠉⠛⢿⣿⣷⡄⠀⠀⠀⠀⠀"
+echo -e "      ⠀⠀             ⢀⣿⣿⡇⠀⠀⣾⣿⡟⠀⠀⢀⣤⣄⠀⠀⠹⣿⣿⡄⠀⠀⠀⠀"
+echo -e " ⠀⠀                  ⣾⣿⣿⡇⠀⠀⢻⣿⣷⡀⠀⠘⣿⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀"
+echo -e "                    ⣼⣿⡿⣿⣿⡄⠀⠈⠻⣿⣿⣷⣿⣿⡿⠃⠀⢀⣿⣿⡇⠀⠀⠀⠀"
+echo -e "                   ⣰⣿⣿⠁⠹⣿⣿⣦⡀⠀⠈⠉⠛⠋⠉⠀⠀⣠⣾⣿⡟⠀⠀⠀⠀⠀"
+echo -e "                   ⣿⣿⣧⣤⣤⣬⣿⣿⣿⣶⣦⣤⣤⣤⣴⣶⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀"
+echo -e "                   ⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀"
+echo -e
+echo -e "           ________________________________________"
+echo -e "          |                                        |"
+echo -e "          |1.DDOS                   7.HOLEHE       |"
+echo -e "          |2.SPAM                   8.YESITSME     |"
+echo -e "          |3.BANED WA               9.OSINT        |"
+echo -e "          |4.UNBANED WA             10.SEEKER      |"
+echo -e "          |5.HACK SOSMED            11.MALWARE     |"
+echo -e "          |6.SPLOIT-MALICIOUS       12.PATHAFINDER |"
+echo -e "          |________________________________________|"
+echo
+read -p "          PILIH MENU:" HOOH
+if [ $HOOH == 1 ]
+then
+clear
+sleep 1
+sudo apt-get update && sudo apt-get updgrade
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install curl
+sudo apt install git
+git clone https://github.com/Whomrx666/anonymous-c2
+cd anonymous-c2
+pip3 install -r requirements.txt
+python3 anonymous-c2.py
+fi
+
+if [ $HOOH == 2 ]
+then
+clear
+sleep 1
+apt install python2
+apt install  git
+ python2 -m pip install requests
+ git clone https://github.com/RozhBasXYZ/OTP
+ cd OTP
+ python2 spam.py
+fi
+
+if [ $HOOH == 3 ]
+then
+clear
+sleep 1
+xdg-open "https://drive.google.com/file/d/152fTXoG9IEsp1unLH47Ap1mV3AvpsYYX/view?usp=drivesdk"
+fi
+
+if [ $HOOH == 4 ]
+then
+clear
+sleep 1
+xdg-open "https://drive.google.com/file/d/14V3b11fDLTNzddSKTdVZiNlDwK7JBTjD/view?usp=drivesdk"
+fi
+
+if [ $HOOH == 5 ]
+clear
+sleep 1
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+cd zphisher
+bash zphisher.sh
+fi
+
+if [ $HOOH == 6 ]
+clear
+sleep 1
+apt-get install git
+apt-get install python2 ruby
+gem install lolcat
+git clone https://github.com/TheSploit/Sploit-Malicious
+cd Sploit-Malicious
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+
+if [ $HOOH == 7 ]
+clear
+sleep 1
+pip3 install holehe
+git clone https://github.com/megadose/holehe.git
+cd holehe/
+python3 setup.py install
+fi
+
+if [ $HOOH == 8 ]
+clear
+sleep 1
+git clone https://github.com/blackeko/yesitsme/
+cd yesitsme
+pip3 install -r requirements.txt
+python3 yesitsme.py -s
+fi
+
+if [ $HOOH == 9 ]
+clear
+sleep 1
+apt update -y && apt upgrade -y
+ apt install python2 -y
+ apt install git
+ git clone https://github.com/Whomrx666/osint-X.git
+ cd osint-X
+ bash module.sh
+ python2 osint-X.py
+fi
+
+if [ $HOOH == 10 ]
+clear
+sleep 1
+git clone https://github.com/thewhiteh4t/seeker.git
+cd seeker/
+python3 seeker.py
+brew install ngrok/ngrok/ngrok
+ngrok http 8080
+python3 seeker.py -h
+fi
+
+if [ $HOOH == 11 ]
+clear
+sleep 1
+sudo apt update
+sudo apt install git python3
+git clone https://github.com/ipansubastian/py-malware-builder
+cd py-malware-builder
+pip3 install -r requirements.txt
+chmod +x main.py
+./main.py --help
+fi
+
+if [ $HOOH == 12 ]
+clear
+sleep 1
+git clone https://github.com/HalilDeniz/PathFinder.git
+pip install -r requirements.txt
+python3 pathFinder.py --help
+fi
